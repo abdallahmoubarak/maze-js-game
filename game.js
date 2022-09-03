9a13fbdda8ace181d8acb22590d0ed43c7b34976
Abdallah <abdallah.moubarak02@gmail.com>
2022-09-04 02:25:18 +0300

    javascript done

---
+++ game.js
@@ -0,0 +1,59 @@
+window.addEventListener("DOMContentLoaded", function () {
+  var game = document.getElementById("game");
+  var start = document.getElementById("start");
+  var end = document.getElementById("end");
+  var status = document.getElementById("status");
+  var boundary = document.getElementsByClassName("boundary");
+  var score = 0;
+  var gameOn = false;
+
+  // event listeners
+  start.addEventListener("mouseover", startGame);
+  game.addEventListener("mouseleave", gameOver);
+  for (var i = 0; i < boundary.length; i++) {
+    boundary[i].addEventListener("mouseover", gameOver);
+  }
+  end.addEventListener("mouseover", endGame);
+
+  // functions
+  function startGame() {
+    gameOn = true;
+  }
+  function gameOver() {
+    if (gameOn) {
+      gameOn = false;
+      updateScore(0);
+    }
+  }
+
+  function endGame() {
+    if (gameOn) {
+      gameOn = false;
+      updateScore(1);
+    }
+  }
+
+  function updateScore(s) {
+    if (s) {
+      score = score + 1;
+    } else {
+      if (score > 0) {
+        score = score - 1;
+      }
+    }
+    updateStatus(s);
+  }
+
+  function updateStatus(state) {
+    var strStat = state ? "won" : "lose";
+    var color = state ? "green" : "red";
+    status.innerHTML =
+      "<div style='display:flex;gap:10rem;justify-content:center;color:" +
+      color +
+      "'><div>Status:" +
+      strStat +
+      "</div><div>Score: " +
+      score +
+      "</div></div>";
+  }
+});
